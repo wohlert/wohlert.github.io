85e8f5f31acdb7eb7972b133aa029a38b3340250
Jesper Wohlert Hansen <jwohlerthansen@macmini.fritz.box>
2026-07-01 19:59:46 +0200

    Deploy static site

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_1Tgihp from "../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcausal-confusion.md";
+import __ASTRO_IMAGE_IMPORT_1PiNXz from "../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Frl-v-sft.md";
+export default new Map([["../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcausal-confusion.md", __ASTRO_IMAGE_IMPORT_1Tgihp], ["../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Frl-v-sft.md", __ASTRO_IMAGE_IMPORT_1PiNXz]]);
+		
